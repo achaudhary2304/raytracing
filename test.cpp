e97d94551f8185271ff73ebe381d56da5e2075f0
Your Name <achaudhary2304@gmail.com>
2024-12-13 16:48:23 +0530

    Intial Commit

---
+++ test.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+
+int main() {
+    std::ofstream outFile("image.ppm");
+
+    int image_width = 15;
+    int image_height = 15;
+
+    outFile << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int i=0;i<image_height;i++){
+        for (int j=0;j<image_width;j++){
+            double a  = (double)i/image_height;
+            double b = (double)j/image_height;
+            double c = 0.0;
+
+            
+            outFile<<(int)(a*259.99)<< " "<<(int)(b*259.99)<<"  "<<(int)(c*259.99)<<"\n";
+
+        }
+    }
+
+    outFile.close();
+    return 0;
+}
